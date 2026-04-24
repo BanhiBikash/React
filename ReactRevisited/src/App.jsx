34e7766b04876b96fcbc37797f9bff4cd30c130f
bunny <banhibikashsaha@gmail.com>
2026-04-25 00:20:38 +0530

    Fetached props data from json file

--- ReactRevisited/src/App.jsx
+++ ReactRevisited/src/App.jsx
@@ -1,5 +1,6 @@
 import Card from "./Components/Card"
 import Navbar from "./Navbar"
+import Data from "./assets/Data.json"
 
 export const App =  () => {
   return (
@@ -7,8 +8,7 @@ export const App =  () => {
         <Navbar />
         <h1>This is React App</h1>
         <h2>This is React Content</h2>
-        <Card name='Banhi' desc='Hi, I am Banhi.' exp={7} imageLink="https://tse3.mm.bing.net/th/id/OIP.j4KdqaXpnhbN94WzVyHUhAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3" />
-        <Card name='Ankit' desc='Hi, I am Ankit.' exp={4} imageLink="https://tse3.mm.bing.net/th/id/OIP.vuiSHQ12eLpH8ZLEYPlDAAHaLI?pid=ImgDet&w=187&h=280&c=7&dpr=1.3&o=7&rm=3" />
+        {Data.map(function(data){return <Card name={data.name} desc={data.exp} exp={data.exp} imageLink={data.imageLink} />})}
     </>
   )
 }
