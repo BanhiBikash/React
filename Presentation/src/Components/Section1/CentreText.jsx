afb5dee675caf0b20b1404adef09887f163ebbc4
bunny <banhibikashsaha@gmail.com>
2026-04-26 01:44:23 +0530

    Learned use state and Set state, also learned destructuring again

--- Presentation/src/Components/Section1/CentreText.jsx
+++ Presentation/src/Components/Section1/CentreText.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const CentreText = () => {
+
+    const [textData, setTextData] = useState({"title":"Prospective customer segmentation","text":"Depending on customer satisfaction and access to banking products,potential target audience can be divided into three groups."})
+
+    const clickedHeader = ()=>{
+        setTextData(prev => {return {...prev,"title":"clicked","text":"We are here to serve"}})
+    }
+
     return (
         <>
             <div className="left-content">
-                <h3>Prospective customer segmentation</h3>
+                <h3 onClick={clickedHeader}>{textData.title}</h3>
                 <p>
-                    Depending on customer satisfaction and access to banking products,
-                    potential target audience can be divided into three groups.
+                    {textData.text}
                 </p>
             </div>
         </>
--- Presentation/src/Components/Section1/Navbar.jsx
+++ Presentation/src/Components/Section1/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Navbar = () => {
+
+  const [v, setV] = useState('Target Audience')
+  
+  function NavClicked(){
+    if(v=='Target Audience')
+    {
+      setV('Audience Targetted')
+    }
+    else{
+      setV('Target Audience')
+    }
+  }
+
   return (
     <div className='flex'>
-      <button>Target Audience</button>
+      <button onClick={NavClicked}>{v}</button>
       <p className='nav-text'>Digital Banking Platform</p>
     </div>
   )
