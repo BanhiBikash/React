2723bf9861c78dbb2ac77d5a12e77e5f44896819
bunny <banhibikashsaha@gmail.com>
2026-04-26 11:02:03 +0530

    Created a form and handled its input with setState

--- Presentation/src/Components/Section2/Section2.jsx
+++ Presentation/src/Components/Section2/Section2.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const Section2 = () => {
+
+  const [inputData, setInputData] = useState('hi')
+
+  const InputDataChange = (event)=>{
+    setInputData(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const submitHandler=(event)=>{
+      event.preventDefault()
+      console.log("submit")
+  }
+
   return (
-    <div className='section2' >Section2</div>
+    <div className='section2' >
+      <form onSubmit={function(event){submitHandler(event)}} action="" className="demo-form">
+        <input type="text" name="form-input" id="form-input" placeholder='Enter anything' value={inputData} onChange={(event)=>{InputDataChange(event)}}/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
