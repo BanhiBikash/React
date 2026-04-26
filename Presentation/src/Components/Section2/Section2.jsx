21635077daa6b0abf182adb1cc57165910681fda
bunny <banhibikashsaha@gmail.com>
2026-04-26 12:12:27 +0530

    got the form data out and store it in the form of json, so that we can send it as request when needed

--- Presentation/src/Components/Section2/Section2.jsx
+++ Presentation/src/Components/Section2/Section2.jsx
@@ -2,22 +2,41 @@ import React,{useState} from 'react'
 
 export const Section2 = () => {
 
+  //handling input
   const [inputData, setInputData] = useState('hi')
 
   const InputDataChange = (event)=>{
     setInputData(event.target.value)
     console.log(event.target.value)
   }
+  
+  const [inputAge, setInputAge] = useState(18)
+
+  const InputAgeChange = (event)=>{
+    setInputAge(event.target.value)
+    console.log(event.target.value)
+  }
+
+
+  //handling form
+
+  const [formData, setformData] = useState({"name":"","age":{}})
 
   const submitHandler=(event)=>{
       event.preventDefault()
-      console.log("submit")
+      setformData(prev=>{return{...prev,name:{inputData},age:{inputAge}}})
+      console.log(formData)
+      setInputData('')
+      setInputAge(0)
   }
 
   return (
     <div className='section2' >
-      <form onSubmit={function(event){submitHandler(event)}} action="" className="demo-form">
-        <input type="text" name="form-input" id="form-input" placeholder='Enter anything' value={inputData} onChange={(event)=>{InputDataChange(event)}}/>
+      <form onSubmit={function(event){submitHandler(event)}} className="demo-form">
+        <label>Name</label>
+        <input type="text" name="Name" id="name-input" placeholder='Enter name' value={inputData} onChange={(event)=>{InputDataChange(event)}}/>
+        <label>Age</label>
+        <input type="number" name="Age" id="name-Age" placeholder='Enter Age' value={inputAge} onChange={(event)=>{InputAgeChange(event)}}/>
         <button type="submit">Submit</button>
       </form>
     </div>
