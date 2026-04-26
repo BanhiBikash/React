1491c813e9ca73b6a2941339556ed226fb8ba054
bunny <banhibikashsaha@gmail.com>
2026-04-26 23:13:51 +0530

    Handled all the inputs of the form as formData instead of individual data-s

--- Presentation/src/Components/Section2/Section2.jsx
+++ Presentation/src/Components/Section2/Section2.jsx
@@ -3,40 +3,47 @@ import React,{useState} from 'react'
 export const Section2 = () => {
 
   //handling input
-  const [inputData, setInputData] = useState('hi')
+  // const [inputData, setInputData] = useState('hi')
 
-  const InputDataChange = (event)=>{
-    setInputData(event.target.value)
-    console.log(event.target.value)
-  }
+  // const InputDataChange = (event)=>{
+  //   setInputData(event.target.value)
+  //   console.log(event.target.value)
+  // }
   
-  const [inputAge, setInputAge] = useState(18)
+  // const [inputAge, setInputAge] = useState(18)
 
-  const InputAgeChange = (event)=>{
-    setInputAge(event.target.value)
-    console.log(event.target.value)
-  }
-
+  // const InputAgeChange = (event)=>{
+  //   setInputAge(event.target.value)
+  //   console.log(event.target.value)
+  // }
 
   //handling form
 
-  const [formData, setformData] = useState({"name":"","age":{}})
+  const [formData, setformData] = useState({"personName":"hi","age":0})
+
+  const formDataChange=(event)=>{
+
+    //get the name and value of chnage target
+    const {name , value} = event.target
+
+    //set the data
+    setformData((prev)=>{return {...prev,[name]:value}})
+    console.log(event.target.value)
+  }
 
   const submitHandler=(event)=>{
       event.preventDefault()
-      setformData(prev=>{return{...prev,name:{inputData},age:{inputAge}}})
       console.log(formData)
-      setInputData('')
-      setInputAge(0)
+      setformData({"personName":"","age":0})
   }
 
   return (
     <div className='section2' >
       <form onSubmit={function(event){submitHandler(event)}} className="demo-form">
         <label>Name</label>
-        <input type="text" name="Name" id="name-input" placeholder='Enter name' value={inputData} onChange={(event)=>{InputDataChange(event)}}/>
+        <input type="text" name="personName" id="name-input" placeholder='Enter name' value={formData.personName} onChange={(event)=>{formDataChange(event)}}/>
         <label>Age</label>
-        <input type="number" name="Age" id="name-Age" placeholder='Enter Age' value={inputAge} onChange={(event)=>{InputAgeChange(event)}}/>
+        <input type="number" name="age" id="name-Age" placeholder='Enter Age' value={formData.age} onChange={(event)=>{formDataChange(event)}}/>
         <button type="submit">Submit</button>
       </form>
     </div>
